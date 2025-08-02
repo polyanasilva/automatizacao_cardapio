bee7e0eac1e3535a8906fa09adf5daa27bd0329b
polyanasilva <araujospolyana@outlook.com>
2025-08-02 00:44:55 -0300

    add formulario

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ import pandas as pd
 from modelo_pdf import gerar_pdf
 
 # Título
-st.title("Gerador de Cardápio do Buffet")
+st.title("Gerador de Proposta - Dois Gastronomia Buffet")
+
+# Informações
+with st.expander("Informações do Evento"):
+    recepcao = st.selectbox("Recepção", ["Almoço", "Janta", "Coffee Break"])
+    local = st.text_input("Local")
+    data_evento = st.text_input("Data do Evento")
+    num_convidados = st.text_input("Número de Convidados")
+    horario = st.text_input("Horário")
+    valor = st.text_input("Valor")
+    data_contrato = st.date_input("Data do Contrato")
 
 # Carregar catálogo
 df = pd.read_csv("data/catalogo.csv")
@@ -19,5 +29,14 @@ for cat in categorias:
 
 # Botão para gerar PDF
 if st.button("Gerar PDF"):
-    gerar_pdf(selecionados)
+    dados_evento = {
+        "recepcao": recepcao,
+        "local": local,
+        "data_evento": data_evento,
+        "num_convidados": num_convidados,
+        "horario": horario,
+        "valor": valor,
+        "data_contrato": data_contrato.strftime("%d de %B de %Y")  # Formata a data
+    }
+    gerar_pdf(selecionados, dados_evento)
     st.success("PDF gerado com sucesso!")
--- modelo_pdf.py
+++ modelo_pdf.py
@@ -1,6 +1,6 @@
 from fpdf import FPDF
 
-def gerar_pdf(dados):
+def gerar_pdf(dados, dados_evento):
     pdf = FPDF()
     
     # Pagina de capa
@@ -14,32 +14,55 @@ def gerar_pdf(dados):
     pdf.image("assets/image2.png", x=(210-60)/2, y=10, w=55, h=40)
 
     # move a posicao para baixo da logo para nao escrever por cima dela
-    pdf.set_xy(10,50)
-
-
+    pdf.set_xy(10,60)
+    
     # 2. Conteudo do cardapio
-    pdf.set_font("Times", size=12)
+    pdf.set_font("Arial", size=10)
+
+    # Informações básicas (agora usando os dados do formulário)
+    pdf.cell(200, 10, txt=f"Estamos enviando proposta para {dados_evento['recepcao'].lower()}", ln=True)
+    pdf.cell(200, 6, txt=f"Local: {dados_evento['local']}", ln=True)
+    pdf.cell(200, 6, txt=f"Data: {dados_evento['data_evento']}", ln=True)
+    pdf.cell(200, 6, txt=f"Número de convidados: {dados_evento['num_convidados']}", ln=True)
+    pdf.cell(200, 6, txt=f"Horário: {dados_evento['horario']}", ln=True)
+    
+    # Cardápio
+    pdf.ln(2)
+    pdf.cell(200, 10, txt="O que iremos servir:", ln=True)
 
     for categoria, itens in dados.items():
         if itens:
-            pdf.ln(10)
-            pdf.set_font("Times", 'B', 14)
-            pdf.cell(200, 10, txt=categoria, ln=True)
-            pdf.set_font("Times", size=12)
+            pdf.ln(1)
+            pdf.set_font("Arial", 'B', 10)
+            if categoria == "Finger Foods" or categoria == "Sobremesas":
+                pdf.cell(200, 5, txt=f'{categoria}: (escolher 02 opções)', ln=True)
+            elif categoria == "Prato Principal":
+                pdf.cell(200, 5, txt=f'{categoria}: (escolher 03 opções)', ln=True)
+            elif categoria == "Bebidas":
+                pdf.cell(200, 5, txt=f'{categoria}: (02 tipos de sucos)', ln=True)
+            else: 
+                pdf.cell(200, 5, txt=f'{categoria}:', ln=True)
+
+            pdf.set_font("Arial", size=10)
             for item in itens:
-                pdf.cell(200, 8, txt=f" - {item}", ln=True)
+                pdf.cell(200, 5, txt=f" - {item}", ln=True)
 
+    pdf.set_font("Arial", 'B')
+    pdf.cell(200, 10, txt="Observação: Já está incluido garçons, copeira, todas as louças e transporte até o local.", ln=True)
 
+    pdf.set_font("Arial")
+    pdf.cell(200, 6, txt=f"- Valor: {dados_evento['valor']}", ln=True)
 
+    pdf.cell(200, 6, txt="- Forma de pagamento 50% adiantado e os outros 50% até 48horas antes da festa. 05:00hs de evento.", ln=True)
+    pdf.cell(200, 6, txt="- Não nos responsabilizamos pela alimentação dos fornecedores da festa.", ln=True)
+    pdf.cell(200, 6, txt="- Validade da proposta: 30 dias.", ln=True)
 
+    pdf.cell(200, 10, txt=f"Belém, {dados_evento['data_contrato']}.", ln=True)
 
     # 3. Adiciona a IMAGEM DO RODAPÉ no fim da página
-    # Altura da imagem: digamos que seja 25 mm
     altura_rodape = 100
-    y_pos = 297 - altura_rodape  # 297 é a altura da página A4
-
+    y_pos = 297 - altura_rodape
     pdf.image("assets/image3.png", x=0, y=y_pos, w=210, h=altura_rodape)
 
     # Salvar PDF
-    pdf.output("outputs/cardapio.pdf")
-
+    pdf.output(f"outputs/cardapio{dados_evento['recepcao'].lower()}.pdf")
